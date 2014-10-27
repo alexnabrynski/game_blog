e1e2281c849269e09073de26581e4acb99aeb6a8
Alex Nabrynski <anabrynski@comcast.net>
2014-10-27 16:59:03 -0700

    finished the CRUD, for reviews and comments.

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,6 +10,7 @@
 #
 
 class Review < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
 					  length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027182828) do
+ActiveRecord::Schema.define(version: 20141027223859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "review_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["review_id"], name: "index_comments_on_review_id", using: :btree
 
   create_table "reviews", force: true do |t|
     t.string   "title"
